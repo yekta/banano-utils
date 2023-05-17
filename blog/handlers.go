759a9b118b640750269cf2c2f08d52a78096496e
yekta <ngyekta@gmail.com>
2023-05-17 14:45:49 +0300

    Remove content type

--- blog/handlers.go
+++ blog/handlers.go
@@ -75,7 +75,6 @@ func GhostToMediumHandler(c *fiber.Ctx) error {
 		log.Fatal(err)
 	}
 	req.Header.Add("Authorization", "Bearer "+MEDIUM_SECRET)
-	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Accept", "*/*")
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
 	resp, err := http.DefaultClient.Do(req)
